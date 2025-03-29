173285be63bf4e95d86ee15a3ad64f2a8f55ea3a
drewlong314 <drewlong314@gmail.com>
2025-03-29 04:55:49 -0500

    add count return

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ import puppeteer from 'puppeteer';
 
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
+let count = 0;
 
 // Navigate to the website
-await page.goto('https://github.com/drewlong314?tab=overview&from=2025-03-01&to=2025-03-29');
+await page.goto('https://github.com/drewlong314');
 
 await page.waitForSelector('[data-date]', { timeout: 10000 });
 
-// Get today's date in the format "YYYY-MM-DD"
-const today = new Date().toISOString().split('T')[0];
+const date = new Date()
 
-const element = await page.evaluate((date) => {
-  return document.querySelector(`[data-date="${date}"]`).getAttribute('data-level');
-}, today);
+let isTrue = true;
+while (isTrue) {
+    const element = await page.evaluate((date) => {
+        return document.querySelector(`[data-date="${date}"]`)?.getAttribute('data-level');
+    }, date.toISOString().split('T')[0]);
 
-if (element) {
-  console.log('Found element:', element);
-} else {
-  console.log('Element not found.');
+    if (element > 0) {
+        console.log('Found element:', element);
+        count++
+        date.setDate(date.getDate() - 1)
+    } else {
+        console.log('Element not found.');
+        isTrue = false
+    }
 }
+console.log(count)
 
 await browser.close();
